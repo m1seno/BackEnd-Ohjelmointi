a3b89f60664bc7aad7b258e100d39d718177c31a
Miika Nordblad <miika.nordblad@myy.haaga-helia.fi>
2025-01-16 17:14:28 +0200

    eka_sovellus alustukset tehty

---
+++ eka_sovellus/src/main/java/syksy24/eka_sovellus/EkaSovellusApplication.java
@@ -0,0 +1,13 @@
+package syksy24.eka_sovellus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkaSovellusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EkaSovellusApplication.class, args);
+	}
+
+}
---
+++ eka_sovellus/src/test/java/syksy24/eka_sovellus/EkaSovellusApplicationTests.java
@@ -0,0 +1,13 @@
+package syksy24.eka_sovellus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EkaSovellusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
