a8eb5ebab54290dbc9f4260fa3c76d42841e27a9
Miika Nordblad <miika.nordblad@myy.haaga-helia.fi>
2025-01-16 18:57:55 +0200

    Ensimmäinen viikkoharjoitus tehty

---
+++ backend/src/main/java/kevat25/backend/web/MyController.java
@@ -0,0 +1,48 @@
+package kevat25.backend.web;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyController {
+
+    @RequestMapping("sayHelloAndAge")
+    @ResponseBody
+    public String returnGreeting(@RequestParam (name="nimesi", required=false, 
+    defaultValue="Tuntematon käyttäjä") String etunimi, @RequestParam int age){
+        return "Hei " + etunimi + ", " + age + " vuotta";
+    }
+
+    @RequestMapping("hello")
+    @ResponseBody
+    public String sayHello(@RequestParam (name="location", required=false,
+    defaultValue="Tuntematon sijainti") String location, @RequestParam (name="name", required=false,
+    defaultValue="Tuntematon käyttäjä") String name){
+
+        List<String> placesRequiringThe = Arrays.asList("Moon", "Sun", "United States, Netherlands");
+
+        if (placesRequiringThe.contains(location)) {
+            location = "the " + location;
+        }
+        
+        return "Welcome to " + location + " " + name + "!";
+    }
+
+    @RequestMapping("index")
+    @ResponseBody
+    public String returnIndexMessage(){
+        return "This is the main page";
+    }
+
+    @RequestMapping("contact")
+    @ResponseBody
+    public String returnContactMessage(){
+        return "This is the contact page";
+    }
+
+}
