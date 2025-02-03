af527041dd57f4fb29a12b73a63b9b8c5f4e6350
Miika Nordblad <miika.nordblad@myy.haaga-helia.fi>
2025-02-03 22:20:35 +0200

    Vko 3, tehtävä 2 (Listpage) tehty

--- bookstore/src/main/java/k25/bookstore/web/BookController.java
+++ bookstore/src/main/java/k25/bookstore/web/BookController.java
@@ -1,20 +1,25 @@
 package k25.bookstore.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import k25.bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
 
-    @GetMapping("/path")
-    public String getPath(@RequestParam String param) {
-        return new String();
+    @SuppressWarnings("FieldMayBeFinal")
+    private BookRepository repository;
+
+    public BookController(BookRepository repository) {
+        this.repository = repository;
     }
 
-    @GetMapping("/index")
-    public String getIndex(@RequestParam String param) {
-        return new String();
+    @RequestMapping("/booklist")
+    public String bookList(Model model) {
+        model.addAttribute("books", repository.findAll());
+        return "booklist";
     }
 
 }
