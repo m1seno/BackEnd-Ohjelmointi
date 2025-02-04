fae34c14b3aed0cc0598e7b27b20f1244e9f32b3
Miika Nordblad <miika.nordblad@myy.haaga-helia.fi>
2025-02-04 23:19:34 +0200

    Vko 3, tehtävä 3 (crud) tehty

--- bookstore/src/main/java/k25/bookstore/web/BookController.java
+++ bookstore/src/main/java/k25/bookstore/web/BookController.java
@@ -2,8 +2,11 @@ package k25.bookstore.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import k25.bookstore.domain.Book;
 import k25.bookstore.domain.BookRepository;
 
 @Controller
@@ -16,10 +19,28 @@ public class BookController {
         this.repository = repository;
     }
 
-    @RequestMapping("/booklist")
+    @GetMapping("/booklist")
     public String bookList(Model model) {
         model.addAttribute("books", repository.findAll());
         return "booklist";
+    }
+
+    @GetMapping("/add")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    @PostMapping("/save")
+    public String saveBook(Book book) {
+        repository.save(book);
+        return "redirect:booklist";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long id) {
+        repository.deleteById(id);
+        return "redirect:../booklist";
     }
 
 }
