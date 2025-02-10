d6c327d4672fad664038cbf3179cf2ab6400d8e1
Miika Nordblad <miika.nordblad@myy.haaga-helia.fi>
2025-02-10 22:19:33 +0200

    Alustettu muutama kategoria mainissa. Lisätty olemassa oleville kirjoille kategoriat

--- bookstore/src/main/java/k25/bookstore/BookstoreApplication.java
+++ bookstore/src/main/java/k25/bookstore/BookstoreApplication.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Bean;
 
 import k25.bookstore.domain.Book;
 import k25.bookstore.domain.BookRepository;
+import k25.bookstore.domain.Category;
+import k25.bookstore.domain.CategoryRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -20,21 +22,32 @@ public class BookstoreApplication {
 	}
 
 	@Bean
-	public CommandLineRunner bookstoreDemo(BookRepository repository) {
+	public CommandLineRunner bookstoreDemo(BookRepository bRepository, CategoryRepository cRepository) {
 		return (args) -> {
 			log.info("save some books");
-			repository.save(new Book("1984", "George Orwell", "9789510505830", 1949, 26.30));
-			repository.save(new Book("Crime and Punishment", "Fyodor Dostoyevsky", "9780140449136", 1866, 25.10));
-			repository.save(new Book("The Brothers Karmazov", "Fyodor Dostoyevsky", "9780140449242", 1880, 24.20));
-			repository.save(new Book("All the Light We Cannot See", "Anthony Doerr", "9780007548699", 2015, 25.00));
-			repository.save(new Book("The Coddling of the American Mind", "Jonathan Haidt", "9780735224896", 2018, 27.50));
+
+			Category category1 = new Category("Fiction");
+			Category category2 = new Category("War");
+			Category category3 = new Category("Psychology");
+
+			cRepository.save(category1);
+			cRepository.save(category2);
+			cRepository.save(category3);
+
+			bRepository.save(new Book("1984", "George Orwell", "9789510505830", 1949, 26.30, category1));
+			bRepository.save(new Book("Crime and Punishment", "Fyodor Dostoyevsky", "9780140449136", 1866, 25.10, category1));
+			bRepository.save(new Book("The Brothers Karmazov", "Fyodor Dostoyevsky", "9780140449242", 1880, 24.20, category1));
+			bRepository.save(new Book("All the Light We Cannot See", "Anthony Doerr", "9780007548699", 2015, 25.00, category1));
+			bRepository.save(new Book("The Coddling of the American Mind", "Jonathan Haidt", "9780735224896", 2018, 27.50, category3));
+			bRepository.save(new Book("Tuntematon Sotilas", "Väinö Linna", "2345098756785", 1954, 31.90, category2));
 
 			log.info("fetch all books");
-			for (Book book : repository.findAll()) {
+			for (Book book : bRepository.findAll()) {
 				log.info(book.toString());
 			}
+
 			log.info("fetch by author");
-			for (Book book : repository.findByAuthor("Fyodor Dostoyevsky")) {
+			for (Book book : bRepository.findByAuthor("Fyodor Dostoyevsky")) {
 				log.info(book.toString());
 			}
 
