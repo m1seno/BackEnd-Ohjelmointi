afc2ddfa5ebf47db07625f8c41776d4b37a88cf4
Miika Nordblad <miika.nordblad@myy.haaga-helia.fi>
2025-02-26 21:26:18 +0200

    WebSecurityConfig -luokka tehty

---
+++ bookstore/src/main/java/k25/bookstore/WebSecurityConfig.java
@@ -0,0 +1,54 @@
+package k25.bookstore;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
+@Configuration
+@EnableMethodSecurity(securedEnabled= true)
+public class WebSecurityConfig {
+
+    private UserDetailsService userDetailsService;
+
+    public WebSecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    private static final AntPathRequestMatcher[] WHITE_LIST_URLS = {
+        new AntPathRequestMatcher("/api/bookrest**"),
+        new AntPathRequestMatcher("/h2-console/**")
+    };
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(
+            authorize -> authorize
+            .requestMatchers(antMatcher("/css/**")).permitAll()
+            .requestMatchers(WHITE_LIST_URLS).permitAll()
+            .anyRequest().authenticated())
+            .headers(headers ->
+            headers.frameOptions(frameOptions -> frameOptions
+            .disable()))
+            .formLogin(formlogin -> 
+            formlogin.loginPage("/login")
+            .defaultSuccessUrl("/booklist", true)
+            .permitAll())
+            .logout(logout -> logout.permitAll())
+            .csrf(csrf -> csrf.disable());
+        return http.build();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+    }
+}
